7a33b1abedf9f97acafabd19483154e649aeb6bf
Feduko18 <164709912+Feduko18@users.noreply.github.com>
2025-11-27 17:30:12 -0300

    Partidos

--- data.js
+++ data.js
@@ -1,31 +1,201 @@
 // data.js - Datos estáticos del torneo
 window.torneoData = {
-  currentYear: 2025,
-  years: {
-    2025: {
-      players: {
-        "Cajote": { name: "Cajote", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Fandiño": { name: "Fandiño", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Fede": { name: "Fede", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Fran Briones": { name: "Fran Briones", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Ger": { name: "Ger", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Girbal": { name: "Girbal", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Juanse": { name: "Juanse", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Lauti": { name: "Lauti", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Palat": { name: "Palat", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Palat Jr": { name: "Pala Jr", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Pela": { name: "Pela", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Salvati": { name: "Salvati", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Samo": { name: "Samo", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Visco": { name: "Visco", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" },
-        "Ziegler": { name: "Ziegler", pj: 0, pg: 0, pp: 0, pts: 0, mundialMG: 0, currentPhase: "groups1", bestPhase: "groups1" }
+  "currentYear": 2025,
+  "years": {
+    "2025": {
+      "players": {
+        "Cajote": {
+          "name": "Cajote",
+          "pj": 1,
+          "pg": 0,
+          "pp": 1,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Fandiño": {
+          "name": "Fandiño",
+          "pj": 1,
+          "pg": 1,
+          "pp": 0,
+          "pts": 3,
+          "mundialMG": 0,
+          "currentPhase": "groups2",
+          "bestPhase": "groups2"
+        },
+        "Fede": {
+          "name": "Fede",
+          "pj": 2,
+          "pg": 1,
+          "pp": 1,
+          "pts": 3,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups2"
+        },
+        "Fran Briones": {
+          "name": "Fran Briones",
+          "pj": 2,
+          "pg": 2,
+          "pp": 0,
+          "pts": 6,
+          "mundialMG": 0,
+          "currentPhase": "round16",
+          "bestPhase": "round16"
+        },
+        "Ger": {
+          "name": "Ger",
+          "pj": 0,
+          "pg": 0,
+          "pp": 0,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Girbal": {
+          "name": "Girbal",
+          "pj": 2,
+          "pg": 0,
+          "pp": 2,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Juanse": {
+          "name": "Juanse",
+          "pj": 2,
+          "pg": 0,
+          "pp": 2,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Lauti": {
+          "name": "Lauti",
+          "pj": 1,
+          "pg": 0,
+          "pp": 1,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Palat": {
+          "name": "Palat",
+          "pj": 0,
+          "pg": 0,
+          "pp": 0,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Palat Jr": {
+          "name": "Pala Jr",
+          "pj": 0,
+          "pg": 0,
+          "pp": 0,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Pela": {
+          "name": "Pela",
+          "pj": 2,
+          "pg": 1,
+          "pp": 1,
+          "pts": 3,
+          "mundialMG": 0,
+          "currentPhase": "groups2",
+          "bestPhase": "groups2"
+        },
+        "Salvati": {
+          "name": "Salvati",
+          "pj": 2,
+          "pg": 0,
+          "pp": 2,
+          "pts": 0,
+          "mundialMG": 0,
+          "currentPhase": "groups1",
+          "bestPhase": "groups1"
+        },
+        "Samo": {
+          "name": "Samo",
+          "pj": 1,
+          "pg": 1,
+          "pp": 0,
+          "pts": 3,
+          "mundialMG": 0,
+          "currentPhase": "groups2",
+          "bestPhase": "groups2"
+        },
+        "Visco": {
+          "name": "Visco",
+          "pj": 2,
+          "pg": 2,
+          "pp": 0,
+          "pts": 6,
+          "mundialMG": 0,
+          "currentPhase": "round16",
+          "bestPhase": "round16"
+        },
+        "Ziegler": {
+          "name": "Ziegler",
+          "pj": 2,
+          "pg": 2,
+          "pp": 0,
+          "pts": 6,
+          "mundialMG": 0,
+          "currentPhase": "round16",
+          "bestPhase": "round16"
+        }
       },
-      matches: [],
-      champions: []
+      "matches": [
+        {
+          "team1": [
+            "Fandiño",
+            "Fede",
+            "Fran Briones",
+            "Visco",
+            "Ziegler"
+          ],
+          "team2": [
+            "Salvati",
+            "Pela",
+            "Lauti",
+            "Juanse",
+            "Girbal"
+          ],
+          "winner": 1,
+          "phase": "groups1",
+          "date": "2025-11-20T20:25:04.966Z"
+        },
+        {
+          "team1": [
+            "Cajote",
+            "Fede",
+            "Salvati",
+            "Juanse",
+            "Girbal"
+          ],
+          "team2": [
+            "Ziegler",
+            "Visco",
+            "Samo",
+            "Pela",
+            "Fran Briones"
+          ],
+          "winner": 2,
+          "phase": "groups2",
+          "date": "2025-11-26T20:28:26.889Z"
+        }
+      ],
+      "champions": []
     }
   }
 };
-
-
-        
-       
